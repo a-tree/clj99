1d567ed068e79c196282ff2d69c9e9c93c29cbd7
a-tree <console.0.25.0.1@gmail.com>
2021-04-02 22:01:03 +0900

    add project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject l99 "0.1.0-SNAPSHOT"
+  :description "Command Line Tool l99"
+  :licence "FIX ME Licence"
+  :url "http://www.example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "0.4.2"]]
+  :aot :all
+  :main l99.core)
+
